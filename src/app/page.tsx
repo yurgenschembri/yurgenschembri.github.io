accdfb8b4ea65dd8a08e3de8667dcfa8c9b86105
Yurgen Schembri <yurgenschembri@gmail.com>
2025-01-12 23:27:37 +0100

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,27 @@ import LinkedInIcon from './components/icons/linkedin-icon';
 
 export default function Home() {
   return (
-    <div className="bg-gray-800 w-screen h-screen flex flex-col items-center justify-center gap-8">
-      <h1 className="text-white">Yurgen Schembri Xuereb</h1>
-      
-      <div className="flex flex-col gap-6 items-center font-s">
-        <sub className="text-gray-400">Web Developer</sub>
-        <sub className="text-gray-400">Software Architect</sub>
-        <sub className="text-gray-400">Cloud Applications Consultant</sub>
-      </div>
-      
-      <div className="text-gray-300 font-mono flex flex-row gap-6 items-center">
-        <a href="mailto:info@yurgenschembri.com" target="_blank" aria-label="Email"
-          className="px-4 py-2 text-blue-100 no-underline bg-sky-500 rounded hover:bg-sky-600 hover:underline group">
-          Get in touch {">"}.
-        </a>
-        <a href="https://www.linkedin.com/in/yurgenschembri/" target="_blank" aria-label="LinkedIn"
-          className="text-sky-500 hover:text-sky-600">
+    <div className="bg-gray-800 w-screen h-screen flex flex-col items-center justify-center">
+      <div className="border-2 border-sky-500/30 border-solid rounded-md p-8 flex flex-col items-center gap-6">
+        <h1 className="text-white">Yurgen Schembri Xuereb</h1>
+
+        <div className="flex flex-col items-center font-extralight text-sm">
+          <a className="text-gray-400" href="#ai">AI Professional</a>
+          <a className="text-gray-400" href="#web">Web Developer</a>
+          <a className="text-gray-400" href="#software">Software Architect</a>
+          <a className="text-gray-400" href="#cloud">Cloud Applications Consultant</a>
+        </div>
+
+        <div className="text-gray-300 font-mono flex flex-row gap-4 items-center">
+          <a href="mailto:info@yurgenschembri.com" target="_blank" aria-label="Email"
+            className="px-4 py-2 no-underline text-blue-100  bg-sky-500 rounded hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-400 transition-colors ease-in-out duration-200">
+            Get in touch {">"}.
+          </a>
+          <a href="https://www.linkedin.com/in/yurgenschembri/" target="_blank" aria-label="LinkedIn"
+            className="text-sky-500 hover:text-sky-600 focus:text-sky-600 active:text-sky-400 p-1">
             <LinkedInIcon />
-        </a>
+          </a>
+        </div>
       </div>
     </div>
   );
