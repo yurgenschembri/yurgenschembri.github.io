3ee5b6619033a5d1ff96618d0d73648096049359
Yurgen Schembri Xuereb <yurgenschembri@gmail.com>
2025-03-09 23:24:50 +0100

    Minor UI improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,38 @@
 import LinkedInIcon from "@/components/icons/linkedin-icon";
+import clsx from "clsx";
 
 export default function Home() {
   return (
-    <div className="bg-wheat w-screen h-screen flex flex-col items-center justify-center">
-      <div className="border-2 border-onyx border-solid rounded-md p-8 flex flex-col items-center gap-6">
+    <div
+      className={clsx(
+        "bg-wheat w-screen h-screen flex flex-col items-center justify-center"
+      )}
+    >
+      <div className="border-0 border-onyx border-solid rounded-md p-8 flex flex-col items-center gap-6">
         <h1 className="text-onyx">Yurgen Schembri Xuereb</h1>
 
-        <div className="flex flex-col items-center font-extralight text-sm">
-          <a className="text-onyx" href="#web">
+        <div className="flex flex-col font-extralight text-sm text-onyx">
+          <a className="text-left" href="#web">
             Web Developer
           </a>
-          <a className="text-onyx" href="#software">
+          <a className="text-center" href="#software">
             Software Architect
           </a>
-          <a className="text-onyx" href="#cloud">
-            Cloud Applications Consultant
+          <a className="text-right" href="#mentorship">
+            Mentor
           </a>
         </div>
 
-        <div className="text-onyx font-mono flex flex-row gap-4 items-center">
+        <div className="text-onyx font-mono flex flex-row gap-2 items-center">
           <a
             href="mailto:info@yurgenschembri.com"
             target="_blank"
             aria-label="Email"
-            className="px-4 py-2 no-underline text-onyx  bg-carolina_blue-500 rounded hover:bg-carolina_blue-600 focus:bg-carolina_blue-600 active:bg-carolina_blue-400 transition-colors ease-in-out duration-200"
+            className={clsx(
+              "h-10 px-4 rounded-sm text-sm content-center no-underline shadow-md text-onyx bg-carolina_blue-500",
+              "transition-colors ease-in-out duration-200",
+              "hover:bg-carolina_blue-600 focus:bg-carolina_blue-600 active:bg-carolina_blue-400"
+            )}
           >
             Get in touch {">"}.
           </a>
@@ -31,6 +40,11 @@ export default function Home() {
             href="https://www.linkedin.com/in/yurgenschembri/"
             target="_blank"
             aria-label="LinkedIn"
+            className={clsx(
+              "h-10 w-10 justify-center flex flex-row p-2 bg-wheat-700 rounded-sm shadow-md",
+              "transition-colors ease-in-out duration-200",
+              "hover:bg-wheat-800"
+            )}
           >
             <LinkedInIcon />
           </a>
--- src/layout/app-nav.tsx
+++ src/layout/app-nav.tsx
@@ -64,21 +64,30 @@ export default function AppNav() {
       <DropdownMenu onOpenChange={(isOpen) => setNavMenuOpen(isOpen)}>
         <DropdownMenuTrigger asChild>
           <Button
-            className={clsx("transition-all duration-100", {
-              "w-12 h-12": navMenuOpen,
-              "w-10 h-10": !navMenuOpen,
-            })}
-            variant="outline"
+            className={clsx(
+              "p-1 bg-wheat-700 group shadow-md",
+              "transition-all ease-in-out duration-100",
+              "hover:bg-wheat-800 focus:bg-wheat-800 data-[state=open]:bg-wheat-800",
+              {
+                "w-12 h-12": navMenuOpen,
+                "w-10 h-10": !navMenuOpen,
+              }
+            )}
           >
             🍔
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="ml-2">
+        <DropdownMenuContent className="ml-2 bg-wheat-700">
           {navMenuItems.map((item) => (
             <Fragment key={item.href}>
               {item.href && !item.items && (
                 <Link href={item.href}>
-                  <DropdownMenuItem className="cursor-pointer h-12">
+                  <DropdownMenuItem
+                    className={clsx(
+                      "h-12 cursor-pointer",
+                      "focus:bg-wheat-800"
+                    )}
+                  >
                     {item.label}
                     <DropdownMenuShortcut>{item.icon}</DropdownMenuShortcut>
                   </DropdownMenuItem>
@@ -86,15 +95,25 @@ export default function AppNav() {
               )}
               {item.items && (
                 <DropdownMenuSub>
-                  <DropdownMenuSubTrigger className="h-12">
+                  <DropdownMenuSubTrigger
+                    className={clsx(
+                      "h-12",
+                      "focus:bg-wheat-800 data-[state=open]:bg-wheat-800"
+                    )}
+                  >
                     {item.label}
                   </DropdownMenuSubTrigger>
                   <DropdownMenuPortal>
-                    <DropdownMenuSubContent className="ml-2 -mt-1">
+                    <DropdownMenuSubContent className="ml-2 -mt-1 bg-wheat-700">
                       {item.items.map((subItem) => (
                         <Fragment key={subItem.href}>
                           <Link href={subItem.href}>
-                            <DropdownMenuItem className="cursor-pointer h-12">
+                            <DropdownMenuItem
+                              className={clsx(
+                                "h-12 cursor-pointer",
+                                "focus:bg-wheat-800"
+                              )}
+                            >
                               {subItem.label}
                               <DropdownMenuShortcut>
                                 {subItem.icon}
